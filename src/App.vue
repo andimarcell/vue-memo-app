55d5a31c80cf72bc9df39c30c2f3c351cbcc59f9
Andi <andimarsituru0064@gmail.com>
2026-04-03 00:44:34 +0800

    Membuat Ui Bagian Form Modal

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,36 @@
       </header>
       <div class="card-container">
         <div class="card">
-          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab, molestiae
+          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab,
+            molestiae
             repellendus, omnis quae quasi eligendi, et fugit ea reprehenderit optio magni consequatur. Nemo ad voluptate
             ab consequatur?</p>
           <p class="card-date">04/03/2026</p>
-          
+
         </div>
-         <div class="card">
-          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab, molestiae
+        <div class="card">
+          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab,
+            molestiae
             repellendus, omnis quae quasi eligendi, et fugit ea reprehenderit optio magni consequatur. Nemo ad voluptate
             ab consequatur?</p>
           <p class="card-date">04/03/2026</p>
-          
+
         </div>
-         <div class="card">
-          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab, molestiae
+        <div class="card">
+          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab,
+            molestiae
             repellendus, omnis quae quasi eligendi, et fugit ea reprehenderit optio magni consequatur. Nemo ad voluptate
             ab consequatur?</p>
           <p class="card-date">04/03/2026</p>
-          
+
         </div>
+      </div>
+    </div>
+    <div class="form-overlay">
+      <div class="form-modal">
+        <button class="form-close-btn">&times;</button>
+        <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
+        <button class="form-save-btn">Save</button>
       </div>
     </div>
   </main>
@@ -83,5 +93,52 @@ header {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-btn {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blue;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-top: 15px;
+  color: white;
+}
+
+.form-close-btn {
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 30px;
+  cursor: pointer;
 }
 </style>
