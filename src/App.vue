8d75b499b1cc4a8429b6d78782221d0871b79c73
Andi <andimarsituru0064@gmail.com>
2026-04-03 00:22:17 +0800

    Membuat Ui Bagian Memo

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,28 @@
         <h1 class="header-title">Memo</h1>
         <button class="header-button">+</button>
       </header>
-      <div>
-        [card]
+      <div class="card-container">
+        <div class="card">
+          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab, molestiae
+            repellendus, omnis quae quasi eligendi, et fugit ea reprehenderit optio magni consequatur. Nemo ad voluptate
+            ab consequatur?</p>
+          <p class="card-date">04/03/2026</p>
+          
+        </div>
+         <div class="card">
+          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab, molestiae
+            repellendus, omnis quae quasi eligendi, et fugit ea reprehenderit optio magni consequatur. Nemo ad voluptate
+            ab consequatur?</p>
+          <p class="card-date">04/03/2026</p>
+          
+        </div>
+         <div class="card">
+          <p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eveniet neque ab, molestiae
+            repellendus, omnis quae quasi eligendi, et fugit ea reprehenderit optio magni consequatur. Nemo ad voluptate
+            ab consequatur?</p>
+          <p class="card-date">04/03/2026</p>
+          
+        </div>
       </div>
     </div>
   </main>
@@ -46,5 +66,22 @@ header {
   border-radius: 100%;
   background-color: aqua;
   color: white;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  padding: 10px;
+  background-color: yellow;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
