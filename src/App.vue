204a630774d3113299dfe0c23cbfa53a9fc56949
Andi <andimarsituru0064@gmail.com>
2026-04-02 08:10:17 +0800

    Membuat UI Memo Bagian Header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,50 @@
 <template>
-  <div>Hello World</div>
+  <main>
+    <div class="container">
+      <header>
+        <h1 class="header-title">Memo</h1>
+        <button class="header-button">+</button>
+      </header>
+      <div>
+        [card]
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 900px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: aqua;
+}
+
+.header-button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  border-radius: 100%;
+  background-color: aqua;
+  color: white;
+}
+</style>
